4bf3bdd16008b35c4032b37ac27727a5355f9ece
ruichen91 <ruichen91@163.com>
2020-03-04 22:37:02 -0500

    add trigram

--- src/run.py
+++ src/run.py
@@ -1,22 +1,25 @@
 import numpy as np
-from utils import read_files, read_predict
-from HMM import *
+from utils import *
+from bigram_hmm import *
+from trigram_hmm import *
 
 def main():
     # training data
-    filename = './POS_train.pos'
+    filename = '../data/POS_dev_train.pos'
+    # transit, emission = read_files_trigram(filename)
     transit, emission = read_files(filename)
     # validation data
-    valid_file = './POS_dev.words'
+    valid_file = '../data/POS_dev.words'
     valid_tokens = read_predict(valid_file)
     # test data
-    test_file = './POS_test.words'
+    test_file = '../data/POS_test.words'
     test_tokens = read_predict(test_file)
 
     # HMM model
-    hmm = HMM(transit, emission)
-    hmm.predict(valid_tokens, './POS_test.pos')
-    a = 0
+    hmm = Bigram_HMM(transit, emission)
+    # hmm = Trigram_HMM(transit, emission)
+    hmm.predict(test_tokens, '../data/POS_test.pos')
+
 
 if __name__ == '__main__':
     main()
--- src/utils.py
+++ src/utils.py
@@ -14,18 +14,18 @@ def read_files(feature_label_file):
     emission_B = {}
 
     keyFile = open(feature_label_file, 'r')
-    key = keyFile.readlines()
+    lines = keyFile.readlines()
 
-    for i in range(len(key)):
+    for i in range(len(lines)):
         # identify each sentence ending
-        key[i] = key[i].rstrip('\n').strip()
-        if key[i] == "":
+        lines[i] = lines[i].rstrip('\n').strip()
+        if lines[i] == "":
             continue
 
-        # each line pasing
-        keyFields = key[i].split('\t')
+        # each lines pasing
+        keyFields = lines[i].split('\t')
         if len(keyFields) != 2:
-            print("format error in key at line " + str(i) + ":" + key[i])
+            print("format error in lines at lines " + str(i) + ":" + lines[i])
             exit()
 
         # get word_POS correspondence
@@ -35,9 +35,9 @@ def read_files(feature_label_file):
 
         # get POS_POS correspondence
         keyPosNext = ""
-        key[i + 1] = key[i + 1].rstrip('\n').strip()
-        if key[i + 1] != "" and key[i + 1] is not None:
-            keyFieldsNext = key[i + 1].split('\t')
+        lines[i + 1] = lines[i + 1].rstrip('\n').strip()
+        if lines[i + 1] != "" and lines[i + 1] is not None:
+            keyFieldsNext = lines[i + 1].split('\t')
             keyPosNext = keyFieldsNext[1]
         update_dict(transit_A, keyPos, keyPosNext)
 
@@ -48,13 +48,65 @@ def read_files(feature_label_file):
     return transit_A, emission_B
 
 
+def read_files_trigram(feature_label_file):
+    transit_A = {}
+    emission_B = {}
+
+    keyFile = open(feature_label_file, 'r')
+    lines = keyFile.readlines()
+
+    # file_size
+    siz = len(lines)
+    for i in range(siz):
+        # identify each sentence ending
+        line0 = lines[i].rstrip('\n').strip()
+        if line0 == "":
+            continue
+
+        # each lines pasing
+        keyFields = line0.split('\t')
+        if len(keyFields) != 2:
+            print("format error in lines at lines " + str(i) + ":" + lines[i])
+            exit()
+
+        # get word_POS correspondence
+        keyToken = keyFields[0]
+        key_current = keyFields[1]
+        update_dict(emission_B, key_current, keyToken)
+
+        # get POS_POS correspondence NEXT_1
+        line1 = ""
+        key_next = ""
+        if i + 1 < siz - 1:
+            line1 = lines[i + 1].rstrip('\n').strip()
+        if line1 != "" and line1 is not None:
+            content_next_1 = line1.split('\t')
+            key_next = content_next_1[1]
+
+        # get Pos_pos correspondence Next_2
+        line2 = ""
+        pos_value = ""
+        if i + 2 < siz - 1:
+            line2 = lines[i + 2].rstrip('\n').strip()
+        if line2 != "" and line2 is not None:
+            content_next_2 = line2.split('\t')
+            pos_value = content_next_2[1]
+        update_dict(transit_A, (key_current, key_next), pos_value)
+
+    # count and get probability
+    prob_update(transit_A)
+    prob_update(emission_B)
+
+    return transit_A, emission_B
+
+
 def prob_update(_dict_):
-    for key, value in _dict_.items():
+    for lines, value in _dict_.items():
         cnt = Counter()
         siz = len(value)
         for word in value:
-            cnt[word] += 1./siz
-        _dict_[key] = dict(cnt)
+            cnt[word] += 1. / siz
+        _dict_[lines] = dict(cnt)
 
 
 def update_dict(_dict_, key, value):
